5288dc66750957643c38375fc4ec720594b6970d
AIchanKimi <1944467655@qq.com>
2025-03-19 16:57:53 +0800

    切换为react

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
-  vue: true,
+  react: true,
   rules: {
     'ts/consistent-type-definitions': ['error', 'type'],
   },
