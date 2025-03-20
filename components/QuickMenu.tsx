9fbcc95da4ed4a952ecdecbc1576c879f2936998
AIchanKimi <1944467655@qq.com>
2025-03-20 15:54:46 +0800

    feat: 更新快速菜单组件，支持动态菜单项和选中文本链接

--- components/QuickMenu.tsx
+++ components/QuickMenu.tsx
@@ -1,23 +1,34 @@
-import { Button } from '@/components/ui/button'
-
-type QuickMenuProps = {
-  select?: string
-  mousePosition: { x: number, y: number }
+export type QuickMenuItem = {
+  label: string
+  link?: string
+  icon?: string
 }
 
-function QuickMenu({ select, mousePosition }: QuickMenuProps) {
-  return (
-    <>
-      <Button
-        className="fixed z-50"
-        style={{
-          top: `${mousePosition.y}px`,
-          left: `${mousePosition.x}px`,
-        }}
-      >
-        {select}
-      </Button>
-    </>
+type QuickMenuProps = {
+  mousePosition: { x: number, y: number }
+  items?: Array<QuickMenuItem>
+  selectedText?: string
+}
+
+function QuickMenu({ mousePosition, items, selectedText }: QuickMenuProps) {
+  return (items && items.length > 0) && (
+    <div
+      className="fixed z-50"
+      style={{
+        top: `${mousePosition.y}px`,
+        left: `${mousePosition.x}px`,
+      }}
+    >
+      {items.map((item) => {
+        const linkUrl = item.link?.replace('{selectedText}', selectedText || '')
+
+        return (
+          <a href={linkUrl} key={item.label} target="_blank" rel="noopener noreferrer">
+            <img src={item.icon} alt="" />
+          </a>
+        )
+      })}
+    </div>
   )
 }
 
--- entrypoints/content/App.tsx
+++ entrypoints/content/App.tsx
@@ -19,7 +19,18 @@ function App() {
     }
   }, [])
 
-  return (<>{select && (<QuickMenu select={select} mousePosition={mousePosition} />)}</>)
+  return (
+    <>
+      {select
+        && (
+          <QuickMenu
+            mousePosition={mousePosition}
+            selectedText={select}
+            items={[{ label: '链接', link: '{selectedText}', icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtbGluayI+PHBhdGggZD0iTTEwIDEzYTUgNSAwIDAgMCA3LjU0LjU0bDMtM2E1IDUgMCAwIDAtNy4wNy03LjA3bC0xLjcyIDEuNzEiLz48cGF0aCBkPSJNMTQgMTFhNSA1IDAgMCAwLTcuNTQtLjU0bC0zIDNhNSA1IDAgMCAwIDcuMDcgNy4wN2wxLjcxLTEuNzEiLz48L3N2Zz4=' }]}
+          />
+        )}
+    </>
+  )
 }
 
 export default App
