12306acf43bc751bec753b4f1beb4095c66d6d47
AIchanKimi <1944467655@qq.com>
2025-04-26 17:44:40 +0800

    chore: 移除Sidebar组件中的标题部分

--- entrypoints/options/components/Sidebar.tsx
+++ entrypoints/options/components/Sidebar.tsx
@@ -26,9 +26,6 @@ export function Sidebar() {
 
   return (
     <div className="bg-card border-r h-full w-64 py-4 flex flex-col flex-shrink-0">
-      <div className="px-4 py-2 mb-6">
-        <h2 className="text-2xl font-bold">OneSearch 设置</h2>
-      </div>
       <nav className="space-y-1 px-2 flex-1">
         {navItems.map(item => (
           <Link
